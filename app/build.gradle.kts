59065a9a3de1c1572a29d8284812707bd95cba48
Jihyo Kim <howiearias@hotmail.com>
2026-09-22 13:40:21 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
+        applicationIdSuffix = ".prototype"
         versionCode = 72
         versionName = "0.17.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
